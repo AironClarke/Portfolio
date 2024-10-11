df18db8149b7a4efb4ade3aa0dca43d7f8822b7e
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-10-11 22:42:39 +0100

    css > styles

--- src/heroFeature/heroFeature.tsx
+++ src/heroFeature/heroFeature.tsx
@@ -1,3 +1,9 @@
-const HeroFeature = (): JSX.Element => <h1>Hello, world!</h1>;
+const HeroFeature = (): JSX.Element => {
+  return (
+    <section className="heroContainer">
+      <h1>Hello, world!</h1>;
+    </section>
+  );
+};
 
 export default HeroFeature;
