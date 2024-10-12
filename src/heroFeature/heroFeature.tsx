82c72b4473a7dd83fb181ead70d0b933e100dbdf
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-10-12 19:59:18 +0100

    importing items to Hero feautre

--- src/heroFeature/heroFeature.tsx
+++ src/heroFeature/heroFeature.tsx
@@ -1,7 +1,13 @@
+import FileManager from 'src/files/FileManager';
+import '../css/heroFeature/heroFeature.css';
+import Taskbar from 'src/components/system/taskbar';
+
 const HeroFeature = (): JSX.Element => {
   return (
     <section className="heroContainer">
       <h1>Hello, world!</h1>;
+      <FileManager />
+      <Taskbar />
     </section>
   );
 };
