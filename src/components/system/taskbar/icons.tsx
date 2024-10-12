4961f0f5570d66372786094f7bf7807445261bd5
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-10-12 21:54:04 +0100

    Added min,max & close icons

--- src/components/system/taskbar/icons.tsx
+++ src/components/system/taskbar/icons.tsx
@@ -1,5 +1,5 @@
 /* eslint import/prefer-default-export: off */
-const WindowIcon = (): JSX.Element => (
+export const WindowIcon = (): JSX.Element => (
   <svg viewBox="0 0 448 512" className="windowSVG">
     <path
       d="M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6
@@ -9,4 +9,20 @@ const WindowIcon = (): JSX.Element => (
   </svg>
 );
 
-export default WindowIcon;
+export const MinimizeIcon = (): JSX.Element => (
+  <svg viewBox="0 0 10 1">
+    <path d="M0 0h10v1H0z" />
+  </svg>
+);
+
+export const MaximizeIcon = (): JSX.Element => (
+  <svg viewBox="0 0 10 10">
+    <path d="M0 0v10h10V0H0zm1 1h8v8H1V1z" />
+  </svg>
+);
+
+export const CloseIcon = (): JSX.Element => (
+  <svg viewBox="0 0 10 10">
+    <path d="M10.2.7L9.5 0 5.1 4.4.7 0 0 .7l4.4 4.4L0 9.5l.7.7 4.4-4.4 4.4 4.4.7-.7-4.4-4.4z" />
+  </svg>
+);
