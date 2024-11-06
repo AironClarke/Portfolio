e58f9d82678afbeb80c8780dc44d71bc8f11e3b5
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-11-06 02:41:50 +0000

    Created dosFunction file

---
+++ src/components/system/apps/JSDOS/dosFunctions.ts
@@ -0,0 +1,34 @@
+import { DosFn } from './DosType';
+
+declare const Dos: DosFn;
+
+export function openMessage() {
+  let dosFinder = document.getElementById('dos') as HTMLDivElement;
+
+  if (!dosFinder) {
+    console.log('dosFinder failed');
+    return;
+  }
+
+  dosFinder.style.display = 'block';
+  Dos(dosFinder, {
+    url: 'https://pub-0f43c31da6244b9c8d180a4b62a89572.r2.dev/duke3D.jsdos',
+    theme: 'light',
+    autoStart: true,
+    noCloud: true,
+    imageRendering: 'smooth',
+    renderAspect: 'Fit'
+  });
+}
+
+export function closeMessage() {
+  let dosFinder = document.getElementById('dos');
+
+  if (!dosFinder) {
+    console.log('dosFinder failed');
+    return;
+  }
+
+  dosFinder.style.display = 'none';
+  Dos(dosFinder).stop();
+}
