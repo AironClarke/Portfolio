3804443f44e3851a150640bdddd5270dc89fb830
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-11-11 05:35:32 +0000

    Added xmlns="http //www.w3.org/2000/svg" to svg files

--- src/components/system/taskbar/icons.tsx
+++ src/components/system/taskbar/icons.tsx
@@ -1,6 +1,10 @@
 /* eslint import/prefer-default-export: off */
 export const WindowIcon = (): JSX.Element => (
-  <svg viewBox="0 0 448 512" className="windowSVG">
+  <svg
+    xmlns="http //www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    className="windowSVG"
+  >
     <path
       d="M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6
      25.3V268.4H0v149.9zm203.8 28L448
@@ -10,19 +14,19 @@ export const WindowIcon = (): JSX.Element => (
 );
 
 export const MinimizeIcon = (): JSX.Element => (
-  <svg viewBox="0 0 10 1">
+  <svg xmlns="http //www.w3.org/2000/svg" viewBox="0 0 10 1">
     <path d="M0 0h10v1H0z" />
   </svg>
 );
 
 export const MaximizeIcon = (): JSX.Element => (
-  <svg viewBox="0 0 10 10">
+  <svg xmlns="http //www.w3.org/2000/svg" viewBox="0 0 10 10">
     <path d="M0 0v10h10V0H0zm1 1h8v8H1V1z" />
   </svg>
 );
 
 export const CloseIcon = (): JSX.Element => (
-  <svg viewBox="0 0 10 10">
+  <svg xmlns="http //www.w3.org/2000/svg" viewBox="0 0 10 10">
     <path d="M10.2.7L9.5 0 5.1 4.4.7 0 0 .7l4.4 4.4L0 9.5l.7.7 4.4-4.4 4.4 4.4.7-.7-4.4-4.4z" />
   </svg>
 );
