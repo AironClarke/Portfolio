be1aecfa561df558d2a3c210fe97bb0b60d6489e
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-11-22 09:40:31 +0000

    Excluded problem dependency (chunk)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    exclude: ['chunk-SB5BK2J2.js?v=3b28f8fd']
+  }
 });
