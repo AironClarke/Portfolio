ecf0692912ba9cf5fb8b85640255a3180810491e
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-11-30 09:15:38 +0000

    Loading screen component

---
+++ src/components/system/common/ContentLoader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContentLoader() {
+  return (
+    <div className="loading-screen">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default ContentLoader;
