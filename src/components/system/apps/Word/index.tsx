cb234d12e52865ab5a207c4f29c1235bbd2a2c47
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-11-30 13:41:52 +0000

    Added content loaded to TinyMCE

--- src/components/system/apps/Word/index.tsx
+++ src/components/system/apps/Word/index.tsx
@@ -2,24 +2,28 @@ import { Editor } from '@tinymce/tinymce-react';
 import config from './config';
 import { useEffect, useState } from 'react';
 import type { Editor as EditorInstance } from 'tinymce';
+import ContentLoader from '../../common/ContentLoader';
 
 const TinyMCE = (): JSX.Element => {
   const [editor, setEditor] = useState<EditorInstance>();
+  const [isLoading, setIsLoading] = useState(true);
 
   const TinyApi = import.meta.env.VITE_TINYNCE;
-  console.log(TinyApi);
 
   return (
     <div className="wordEditor">
+      {isLoading && <ContentLoader />}
       <Editor
         apiKey={TinyApi}
-        onInit={(_event, activeEditor) => setEditor(activeEditor)}
+        onInit={(_event, activeEditor) => {
+          setEditor(activeEditor);
+          setIsLoading(false); // Hide loader when editor is ready
+        }}
         init={{
           ...config,
-          plugins: 'lists link', // Add the plugins you need
-          menubar: false, // Disable menubar for faster load
-          branding: false, // Disable TinyMCE branding
-          statusbar: false // Disable statusbar
+          plugins: 'lists link',
+          branding: false,
+          statusbar: false
         }}
       />
     </div>
