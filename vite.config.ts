b62d694ec57eeff53f6e06254f8acd64af2dee35
Airon <131386167+AironClarke@users.noreply.github.com>
2025-01-31 20:19:39 +0000

    Added tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from "path"
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -7,5 +8,10 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     exclude: ['chunk-SB5BK2J2.js?v=3b28f8fd']
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   }
 });
