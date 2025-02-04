2228ebe4d5830fcd3f2ce32285210c662e4940b8
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-04 21:12:04 +0000

    Implemented shadcn

--- src/components/system/apps/Spotify/frontend/App.tsx
+++ src/components/system/apps/Spotify/frontend/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
     <>
       <h1 className="text-3x1 font-bold underline">Hello</h1>
+      <Button className="bg-red-100">This is a button</Button>
     </>
   )
 }
