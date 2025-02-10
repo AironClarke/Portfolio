ed116e9ea3d14e30a47961bd2794fb3e2859e836
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-10 01:17:58 +0000

    MainLayout component

--- src/components/system/apps/Spotify/frontend/App.tsx
+++ src/components/system/apps/Spotify/frontend/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/Homepage"
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
+import MainLayout from "./layout/MainLayout"
 
 
 function App() {
@@ -12,6 +13,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback/>} />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />} >
+          <Route path="/" element={<HomePage />} />
+        </Route>
       </Routes>
     </>
   )
