289891f4e1b966fe0ad90bfd52f05aa3f36cc84c
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-10 09:07:12 +0000

    Mainlayout with added resizable pannels

--- src/components/system/apps/Spotify/frontend/App.tsx
+++ src/components/system/apps/Spotify/frontend/App.tsx
@@ -3,6 +3,8 @@ import HomePage from "./pages/home/Homepage"
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
 import MainLayout from "./layout/MainLayout"
+import ChatPage from "./pages/chat/chatPage"
+import "index.css"
 
 
 function App() {
@@ -10,12 +12,13 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+
         <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback/>} />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
 
         <Route element={<MainLayout />} >
           <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
         </Route>
       </Routes>
     </>
