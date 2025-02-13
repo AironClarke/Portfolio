ff4b45e12bfcd23803824a45b1360a6337e193b2
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-13 20:42:53 +0000

    Added song image and title to the sectionGrid for homePage

--- src/components/system/apps/Spotify/frontend/pages/home/components/SectionGrid.tsx
+++ src/components/system/apps/Spotify/frontend/pages/home/components/SectionGrid.tsx
@@ -8,7 +8,7 @@ type SectionGridProps = {
   isLoading: boolean
 }
 
-const SectionGrid = ({ songs,title,isLoading}:SectionGridProps ) => {
+const SectionGrid = ({ songs,title,isLoading }:SectionGridProps ) => {
 
   if(isLoading) return <SectionGridSkeleton />
 
@@ -20,6 +20,28 @@ const SectionGrid = ({ songs,title,isLoading}:SectionGridProps ) => {
           Show all
         </Button>
       </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+				{songs.map((song) => (
+					<div
+						key={song._id}
+						className='bg-zinc-800/40 p-4 rounded-md hover:bg-zinc-700/40 transition-all group cursor-pointer'
+					>
+						<div className='relative mb-4'>
+							<div className='aspect-square rounded-md shadow-lg overflow-hidden'>
+								<img
+									src={song.imageUrl}
+									alt={song.title}
+									className='w-full h-full object-cover transition-transform duration-300
+									group-hover:scale-105'
+								/>
+							</div>
+						</div>
+						<h3 className='font-medium mb-2 truncate'>{song.title}</h3>
+						<p className='text-sm text-zinc-400 truncate'>{song.artist}</p>
+					</div>
+				))}
+			</div>
 
 
     </div>
