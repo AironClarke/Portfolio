72c7054c517c42f9ee2a926dcaf509f12759485c
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-13 23:27:38 +0000

    Play and stop function to album page

--- src/components/system/apps/Spotify/frontend/layout/MainLayout.tsx
+++ src/components/system/apps/Spotify/frontend/layout/MainLayout.tsx
@@ -2,11 +2,13 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/componen
 import { Outlet } from "react-router-dom"
 import LeftSidebar from "./components/LeftSidebar"
 import FriendsActivty from "./components/FriendsActivity"
+import AudioPlayer from "./components/AudioPlayer"
 
 const MainLayout = () => {
   const isMobile = false
   return (
     <div className="h-screen bg-black text-white flex flex-col">
+      <AudioPlayer />
       <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow">
         {/* { left sidebar } */}
         <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
