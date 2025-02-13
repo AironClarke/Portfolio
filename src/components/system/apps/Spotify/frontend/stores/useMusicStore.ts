612fb1fd4959a19eab4db2edbe1257d2a5fcafe4
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-13 00:34:01 +0000

    fetch functions

--- src/components/system/apps/Spotify/frontend/stores/useMusicStore.ts
+++ src/components/system/apps/Spotify/frontend/stores/useMusicStore.ts
@@ -1,16 +1,24 @@
 import { axiosInstance } from "@/lib/axios"
 import {create} from "zustand"
 import { Album, Song } from "../types";
+import { resolveTripleslashReference } from "typescript";
 
 interface MusicStore {
   songs: Song[];
   albums: Album[];
   isLoading: boolean;
   error: string | null;
-  currentAlbum: Album | null,
+  currentAlbum: Album | null;
+  featuredSongs: Song[];
+  madeForYouSongs: Song[];
+  trendingSongs: Song[];
 
   fetchAlbums: () => Promise<void>;
-  fetchAlbumById: (id: string) => Promise<void>
+  fetchAlbumById: (id: string) => Promise<void>;
+  fetchFeaturedSongs: () => Promise<void>;
+  fetchMadeForYouSongs: () => Promise<void>;
+  fetchTrendingSongs: () => Promise<void>
+
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
@@ -19,6 +27,9 @@ export const useMusicStore = create<MusicStore>((set) => ({
   isLoading: false,
   error: null,
   currentAlbum: null,
+  featuredSongs: [],
+  madeForYouSongs: [],
+  trendingSongs: [],
 
   fetchAlbums: async () => {
     set({isLoading: true,error: null})
@@ -43,6 +54,45 @@ export const useMusicStore = create<MusicStore>((set) => ({
     } finally {
       set({ isLoading: false })
     }
-  }
+  },
+
+  fetchFeaturedSongs: async( ) => {
+    set({ isLoading: true, error: null })
+    try {
+      const response = await axiosInstance.get("songs/featured")
+      set({ featuredSongs: response.data })
+
+    } catch (error: any) {
+      set({ error: error.response.data.message })
+
+    } finally {
+      set({ isLoading: false })
+    }
+
+  },
+  fetchMadeForYouSongs: async() => {
+    set({ isLoading: true, error: null })
+    try {
+      const response = await axiosInstance.get("/songs/made-for-you")
+      set({ madeForYouSongs: response.data })
+    } catch (error: any) {
+      set({ error: error.response.data.message})
+    } finally {
+      set({ isLoading: false })
+    }
+  },
+
+
+  fetchTrendingSongs: async() => {
+    set({ isLoading: true, error: null})
+    try {
+      const response = await axiosInstance.get("/songs/trending")
+      set({ trendingSongs: response.data })
+    } catch (error: any) {
+      set({ error: error.response.data.message})
+    } finally {
+      set({ isLoading: false })
+    }
+  },
 
 }))
