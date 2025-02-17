dc6205267d6e358c939df9c13a755c35d9c50bd4
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-17 13:37:54 +0000

    bugfix songs now upload correctly

--- src/components/system/apps/Spotify/backend/controller/admin.controller.js
+++ src/components/system/apps/Spotify/backend/controller/admin.controller.js
@@ -15,43 +15,41 @@ const uploadToCloudinary = async (file) => {
 }
 
 export const createSong = async (req, res, next) => {
-  try {
-    if (!req.files || !req.files.audioFiles || !req.files.imageFiles) {
-      return res.status(400).json({ messgae: "Please upload all files"})
-    }
+	try {
+		if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+			return res.status(400).json({ message: "Please upload all files" });
+		}
 
-    const { title, artist, albumId, duration} = req.body
-    const audioFiles = req.files.audioFiles
-    const imageFiles = req.files.imageFiles
+		const { title, artist, albumId, duration } = req.body;
+		const audioFile = req.files.audioFile;
+		const imageFile = req.files.imageFile;
 
-    const audioUrl = await uploadToCloudinary(audioFiles)
-    const imageUrl = await uploadToCloudinary(imageFiles)
+		const audioUrl = await uploadToCloudinary(audioFile);
+		const imageUrl = await uploadToCloudinary(imageFile);
 
-    const song = new Song({
-      title,
-      artist,
-      audioUrl,
-      imageUrl,
-      duration,
-      albumId: albumId || null
-    })
+		const song = new Song({
+			title,
+			artist,
+			audioUrl,
+			imageUrl,
+			duration,
+			albumId: albumId || null,
+		});
 
-    await song.save()
+		await song.save();
 
-    // if file belongs to an album, update the songs array
-    if(albumId){
-      await Album.findbyIdAndUpdate(albumId, {
-        $push: { songs: song._id}
-      })
-    }
-
-    res.status(201).json(song)
-
-  } catch (error) {
-    console.log("Error in createSong", error)
-    next(error)
-  }
-}
+		// if song belongs to an album, update the album's songs array
+		if (albumId) {
+			await Album.findByIdAndUpdate(albumId, {
+				$push: { songs: song._id },
+			});
+		}
+		res.status(201).json(song);
+	} catch (error) {
+		console.log("Error in createSong", error);
+		next(error);
+	}
+};
 
 export const deleteSong = async (req, res, next) => {
   try {
