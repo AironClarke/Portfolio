c206f18532405104fadf3ce88d4a9a95203eb20e
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-17 13:15:33 +0000

    Added album routes

--- src/components/system/apps/Spotify/backend/routes/admin.route.js
+++ src/components/system/apps/Spotify/backend/routes/admin.route.js
@@ -1,17 +1,17 @@
 import { Router } from "express"
-import { createSong, deleteSong, checkAdmin } from "../controller/admin.controller.js"
+import { checkAdmin, createAlbum, createSong, deleteAlbum, deleteSong } from "../controller/admin.controller.js"
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.use(protectRoute, checkAdmin) // applies this to all routes
+router.use(protectRoute, requireAdmin) // applies this to all routes
 
 router.get("/check", checkAdmin)
 
 router.post("/songs", createSong)
 router.delete("/songs/:id", deleteSong)
 
-router.post("/albums", createSong)
-router.delete("/albums/:id", deleteSong)
+router.post("/albums", createAlbum)
+router.delete("/albums/:id", deleteAlbum)
 
 export default router
