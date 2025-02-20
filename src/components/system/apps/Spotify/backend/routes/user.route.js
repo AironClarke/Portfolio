e6633e5c73725633fe990b97cfcff128225b92b8
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-20 08:40:40 +0000

    Removed like route

--- src/components/system/apps/Spotify/backend/routes/user.route.js
+++ src/components/system/apps/Spotify/backend/routes/user.route.js
@@ -1,14 +1,11 @@
 import { Router } from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
-import { getAllUsers } from "../controller/user.controller.js";
+import { getAllUsers, getMessages } from "../controller/user.controller.js";
 
 const router = Router();
 
-router.get("/like", protectRoute, (req,res) => {
-  req.auth.userId
-  res.send("User route with GET method")
-})
-
 router.get("/", protectRoute, getAllUsers)
+
+router.get("/messages/:userId", protectRoute, getMessages)
 
 export default router
