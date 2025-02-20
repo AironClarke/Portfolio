741080a0920ada4910ab7e4c799e869e0079600d
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-20 17:23:01 +0000

    Online indercator for friends

--- src/components/system/apps/Spotify/frontend/pages/chat/components/UserList.tsx
+++ src/components/system/apps/Spotify/frontend/pages/chat/components/UserList.tsx
@@ -1,6 +1,8 @@
 import { ScrollArea } from "@radix-ui/react-scroll-area"
 import { useChatStore } from "../../../stores/useChatStore"
 import UserListSkeleton from "@/components/ui/skeletons/UserListSkeleton"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { AvatarFallback } from "@radix-ui/react-avatar"
 
 const UserList = () => {
   const { users, selectedUser, isLoading, setSelectedUser, onlineUsers } = useChatStore()
@@ -21,6 +23,20 @@ const UserList = () => {
 									rounded-lg cursor-pointer transition-colors
                   ${selectedUser?.clerkId === user.clerkId ? "bg-zinc-800" : "hover:bg-zinc-800/50"}`}
                 >
+                  <div className="relative">
+                    <Avatar className="size-8 md:size-12">
+                      <AvatarImage src={user.imageUrl} />
+                      <AvatarFallback>{user.fullName[0]}</AvatarFallback>
+                    </Avatar>
+                    {/* { online indicator } */}
+                    <div className={`absolute bottom-0 right-0 h-3 w-3 rounded-full ring-2 ring-zinc-900
+                      ${onlineUsers.has(user.clerkId) ? "bg-green-500" : "bg-zinc-500"}`
+                    } />
+                  </div>
+
+                  <div className="flex-1 min-w-0 lg:block hidden">
+                    <span className="font-medium truncate">{user.fullName}</span>
+                  </div>
 
                 </div>
               ))
