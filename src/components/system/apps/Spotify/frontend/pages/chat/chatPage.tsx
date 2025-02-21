c48b4a6f5cd43ca71eb0a3aa22b7b756db7ab34e
Airon <131386167+AironClarke@users.noreply.github.com>
2025-02-21 13:00:52 +0000

    timestap for message sent

--- src/components/system/apps/Spotify/frontend/pages/chat/chatPage.tsx
+++ src/components/system/apps/Spotify/frontend/pages/chat/chatPage.tsx
@@ -8,6 +8,16 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import MessageInput from "./components/MessageInput"
 
+const formatTime = (date: string) => {
+  return (
+    new Date(date).toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true
+    })
+  )
+}
+
 const ChatPage = () => {
   const {user} = useUser()
   const { messages, selectedUser, fetchUsers, fetchMessages} = useChatStore()
@@ -58,7 +68,7 @@ const ChatPage = () => {
                       >
                         <p className="text-sm">{message.content}</p>
                         <span className="text-xs text-zinc-300 mt-1 block">
-                          {/* {formatTime(message.createdAt)} */}
+                          {formatTime(message.createdAt)}
                         </span>
                       </div>
                     </div>
